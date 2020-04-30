1aa5aec66e8a81873bd7aa5a9295a47cafdd00c1
bh2o <tenev.bh2o@gmail.com>
2020-04-30 12:15:41 +0300

    Add Spring.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# energy-price-calculator-api
+# Energy price calculator API
 Price calculation service and API for average energy price calculation in selected period.
---
+++ src/main/java/com/example/energypricecalculatorapi/EnergyPriceCalculatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.energypricecalculatorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyPriceCalculatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyPriceCalculatorApiApplication.class, args);
+	}
+
+}
